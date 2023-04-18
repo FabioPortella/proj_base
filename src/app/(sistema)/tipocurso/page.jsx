7c4e8472b5c8fcf60181e8688070932ce19f05a9
FabioPortella <fabioportella2009@gmail.com>
2023-04-18 11:12:56 -0300

    iniciando projeto

---
+++ src/app/(sistema)/tipocurso/page.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+    title: 'Tipo de Curso'
+}
+
+export default function Page(){
+    return(
+        <>
+            <h2>Tipo de Curso</h2>
+            <h4>Exemplo de CRUD para Tipo de Curso</h4>
+        </>
+    )
+}
